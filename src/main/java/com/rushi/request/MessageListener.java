6358a145a26befb55a0e4f258b1a0c66b0f12338
Prathamesh Gulave <144264348+rushii18@users.noreply.github.com>
2024-01-31 17:05:36 +0530

    Update MessageListener.java

--- src/main/java/com/rushi/request/MessageListener.java
+++ src/main/java/com/rushi/request/MessageListener.java
@@ -13,16 +13,8 @@ public class MessageListener {
 	@Autowired
 	SimpMessagingTemplate template;
 
-//	@KafkaListener(
-//			topics = KafkaConstants.KAFKA_TOPIC,
-//			groupId = KafkaConstants.GROUP_ID
-//			
-//			)
-	
-	
-	
-	
-	    public void listen(Message message) {
+
+	public void listen(Message message) {
 	        System.out.println("sending via kafka listener..");
 	        template.convertAndSend("/topic/group", message);
 	    }
